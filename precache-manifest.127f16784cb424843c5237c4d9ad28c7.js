3f50a524291df153d29d3604b284f10f7b11badd
skowronpatryk <skowron.patr@gmail.com>
2019-09-22 19:38:01 +0200

    Updates

---
+++ precache-manifest.127f16784cb424843c5237c4d9ad28c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5df04a5937602c95bca19aa9ac50c7",
+    "url": "/skowronpatryk/ReactAPP/index.html"
+  },
+  {
+    "revision": "edacb1ce74704b2ba22b",
+    "url": "/skowronpatryk/ReactAPP/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "54412f96ae3503fc0b5e",
+    "url": "/skowronpatryk/ReactAPP/static/css/main.0747f39b.chunk.css"
+  },
+  {
+    "revision": "edacb1ce74704b2ba22b",
+    "url": "/skowronpatryk/ReactAPP/static/js/2.4f9df473.chunk.js"
+  },
+  {
+    "revision": "54412f96ae3503fc0b5e",
+    "url": "/skowronpatryk/ReactAPP/static/js/main.f58ba5c6.chunk.js"
+  },
+  {
+    "revision": "77f79458ef33bc406e06",
+    "url": "/skowronpatryk/ReactAPP/static/js/runtime~main.c4ce25bb.js"
+  },
+  {
+    "revision": "a68ffee2c4be8e86911bc6330631b612",
+    "url": "/skowronpatryk/ReactAPP/static/media/cheese.a68ffee2.png"
+  },
+  {
+    "revision": "57883a77bac42d326729153331450378",
+    "url": "/skowronpatryk/ReactAPP/static/media/header-pizza.57883a77.jpg"
+  },
+  {
+    "revision": "bf3e408474916b7658060384cc653733",
+    "url": "/skowronpatryk/ReactAPP/static/media/mushrooms.bf3e4084.png"
+  },
+  {
+    "revision": "321621814a9ff2e1c61bf2a7c3c3f6da",
+    "url": "/skowronpatryk/ReactAPP/static/media/pizza.32162181.png"
+  },
+  {
+    "revision": "4684e4ec5a831f1fc3db3d0d5b6cb4a8",
+    "url": "/skowronpatryk/ReactAPP/static/media/rukola.4684e4ec.png"
+  },
+  {
+    "revision": "1580be39bab69f026b327b5b23b1ffb5",
+    "url": "/skowronpatryk/ReactAPP/static/media/salami.1580be39.png"
+  },
+  {
+    "revision": "c33c9f5915504df8f38e28e3d042e7d4",
+    "url": "/skowronpatryk/ReactAPP/static/media/tomato.c33c9f59.png"
+  }
+]);
